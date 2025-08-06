ba95eedbc30ad4e53b711fcd645d155449614f54
Irenimoyan <sheutomalli@gmail.com>
2025-08-06 15:55:42 +0100

    added a user profile with styling

---
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -0,0 +1,13 @@
+function Userprofile() {
+    return (
+        <div className=" bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+            <img className="rounded-full w-36 h-36 mx-auto" src="https://via.placeholder.com/150" alt="User" />
+            <h1 className=" text-lg text-blue-800 my-400">John Doe</h1>
+            <p className="text-gray-600 text-base">Developer at Example Co. loves to write code,
+                and explore new technologies
+            </p>
+        </div>
+    );
+}
+
+export default Userprofile
