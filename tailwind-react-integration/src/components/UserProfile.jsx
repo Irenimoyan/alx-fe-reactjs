bf1f94761a808bf5424b5b9540167929a79ef41e
Irenimoyan <sheutomalli@gmail.com>
2025-08-06 16:18:19 +0100

    import userProfile into the App.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,7 +1,9 @@
 function Userprofile() {
     return (
         <div className=" bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
-            <img className="rounded-full w-36 h-36 mx-auto" src="https://via.placeholder.com/150" alt="User" />
+            <img className="rounded-full w-36 h-36 mx-auto"
+                src="./img/userimg" alt=""
+            />
             <h1 className=" text-lg text-blue-800 my-400">John Doe</h1>
             <p className="text-gray-600 text-base">Developer at Example Co. loves to write code,
                 and explore new technologies
